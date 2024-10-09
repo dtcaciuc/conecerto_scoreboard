e2cb10596cc15eb087d8d4d821b7f210702ae619
Dimitri Tcaciuc <dtcaciuc@gmail.com>
2024-10-08 20:16:20 -0700

    brands: Add endpoint path prefix to asset URLs.
    Refactor and simplify the code.

--- lib/conecerto_scoreboard_web/brands.ex
+++ lib/conecerto_scoreboard_web/brands.ex
@@ -1,6 +1,10 @@
 defmodule Conecerto.ScoreboardWeb.Brands do
   use GenServer
 
+  require Logger
+
+  @endpoint Conecerto.ScoreboardWeb.Endpoint
+
   def start_link(args) do
     {name, args} = Keyword.pop(args, :name, __MODULE__)
     GenServer.start_link(__MODULE__, args, name: name)
@@ -20,54 +24,61 @@ defmodule Conecerto.ScoreboardWeb.Brands do
     do: {:ok, %{organizer: nil, sponsors: []}}
 
   def init(asset_dir: asset_dir) do
-    {timestamp, _usecs} = NaiveDateTime.utc_now() |> NaiveDateTime.to_gregorian_seconds()
+    files = list_images(asset_dir)
 
-    {organizer, sponsors} =
-      case File.ls(asset_dir) do
-        {:ok, names} ->
-          {find_organizer(names, asset_dir, timestamp),
-           find_sponsors(names, asset_dir, timestamp)}
-
-        {:error, :enoent} ->
-          raise File.Error, "Branding asset directory #{asset_dir} does not exist"
-      end
+    organizer = Enum.find(files, &organizer?(&1.name))
+    sponsors = Enum.reject(files, &organizer?(&1.name))
 
     {:ok, %{organizer: organizer, sponsors: sponsors}}
   end
 
   @impl true
   def handle_call(:get_organizer, _from, state),
-    do: {:reply, state.organizer, state}
+    do: {:reply, put_url(state.organizer), state}
 
   @impl true
   def handle_call(:get_sponsors, _from, state),
-    do: {:reply, state.sponsors, state}
+    do: {:reply, Enum.map(state.sponsors, &put_url/1), state}
 
   @impl true
   def handle_call(:any?, _from, state),
     do: {:reply, state.organizer != nil || Enum.count(state.sponsors) > 0, state}
 
-  defp find_organizer(names, asset_dir, timestamp) do
-    names
-    |> Enum.find(&(image?(&1) and organizer?(&1)))
-    |> make_entry(asset_dir, timestamp)
+  defp list_images(asset_dir) do
+    case File.ls(asset_dir) do
+      {:ok, names} ->
+        names
+        |> Enum.sort()
+        |> Enum.map(fn name ->
+          path = Path.join(asset_dir, name)
+
+          case file_hash(path) do
+            {:ok, hash} ->
+              %{name: name, path: path, hash: hash}
+
+            _ ->
+              Logger.error("Could not hash #{path}; skipping")
+              nil
+          end
+        end)
+        |> Enum.filter(&(&1 != nil and image?(&1.name)))
+
+      {:error, :enoent} ->
+        Logger.error("Brands directory #{asset_dir} does not exist")
+        []
+
+      {:error, error} ->
+        Logger.error("Could not list brands directory #{asset_dir} contents - #{inspect(error)}")
+        []
+    end
   end
 
-  defp find_sponsors(names, asset_dir, timestamp) do
-    names
-    |> Enum.filter(&(image?(&1) and not organizer?(&1)))
-    |> Enum.sort()
-    |> Enum.map(&make_entry(&1, asset_dir, timestamp))
-  end
-
-  defp make_entry(nil = _logo_filename, _asset_dir, _timestamp),
+  # Adding URL dynamically since we start up before the endpoint does.
+  defp put_url(nil),
     do: nil
 
-  defp make_entry(logo_filename, asset_dir, timestamp),
-    do: %{
-      url: "/brands/#{logo_filename}?v=#{timestamp}",
-      path: Path.join(asset_dir, logo_filename)
-    }
+  defp put_url(entry),
+    do: Map.put(entry, :url, @endpoint.path("/brands/#{entry.name}?v=#{entry.hash}"))
 
   defp organizer?(path),
     do: Path.rootname(path) == "organizer"
@@ -75,5 +86,11 @@ defmodule Conecerto.ScoreboardWeb.Brands do
   defp image?(path) do
     ext = Path.extname(path)
     ext == ".png" or ext == ".jpg"
+  end
+
+  defp file_hash(path) do
+    with {:ok, content} <- File.read(path) do
+      {:ok, :crypto.hash(:md5, content) |> Base.encode16() |> String.downcase()}
+    end
   end
 end
