211d55c1c54e73e075deabda74a34f2d78755333
Dimitri Tcaciuc <dtcaciuc@gmail.com>
2024-11-01 22:52:18 -0700

    Fix tests.

--- test/conecerto_scoreboard_web/brands_test.exs
+++ test/conecerto_scoreboard_web/brands_test.exs
@@ -22,11 +22,11 @@ defmodule Conecerto.ScoreboardWeb.BrandsTest do
 
   @tag :tmp_dir
   test "handle one or more sponsors", %{tmp_dir: tmp_dir} do
-    s1_path = Path.join(tmp_dir, "a-sponsor.jpg")
-    s2_path = Path.join(tmp_dir, "b-sponsor.png")
+    name1 = "a-sponsor.jpg"
+    name2 = "b-sponsor.png"
 
-    File.write!(s1_path, "")
-    File.write!(s2_path, "")
+    File.write!(Path.join(tmp_dir, name1), "")
+    File.write!(Path.join(tmp_dir, name2), "")
     File.write!(Path.join(tmp_dir, "c-sponsor.notanimage"), "")
 
     pid = start_supervised!({Brands, [asset_dir: tmp_dir, name: nil]})
@@ -35,25 +35,25 @@ defmodule Conecerto.ScoreboardWeb.BrandsTest do
     assert nil == GenServer.call(pid, :get_organizer)
     assert [s1, s2] = GenServer.call(pid, :get_sponsors)
 
-    assert s1.path == s1_path
-    assert String.starts_with?(s1.url, "/brands/a-sponsor.jpg?v=")
+    assert s1.name == name1
+    assert String.starts_with?(s1.path, "/brands/a-sponsor.jpg?v=")
 
-    assert s2.path == s2_path
-    assert String.starts_with?(s2.url, "/brands/b-sponsor.png?v=")
+    assert s2.name == name2
+    assert String.starts_with?(s2.path, "/brands/b-sponsor.png?v=")
   end
 
   @tag :tmp_dir
   test "handle organizer", %{tmp_dir: tmp_dir} do
-    org_path = Path.join(tmp_dir, "organizer.jpg")
-    File.write!(org_path, "")
+    name = "organizer.jpg"
+    File.write!(Path.join(tmp_dir, name), "")
 
     pid = start_supervised!({Brands, [asset_dir: tmp_dir, name: nil]})
 
     assert GenServer.call(pid, :any?)
-    assert %{path: path, url: url} = GenServer.call(pid, :get_organizer)
+    assert %{name: name, path: path} = GenServer.call(pid, :get_organizer)
     assert [] = GenServer.call(pid, :get_sponsors)
 
-    assert path == org_path
-    assert String.starts_with?(url, "/brands/organizer.jpg?v=")
+    assert name == name
+    assert String.starts_with?(path, "/brands/organizer.jpg?v=")
   end
 end
