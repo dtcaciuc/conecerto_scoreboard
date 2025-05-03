9e9af9d6b22504c9963ab946747f1f62cce2f6d7
Dimitri Tcaciuc <dtcaciuc@gmail.com>
2025-05-02 23:19:37 -0700

    new version 0.9.0-rc.15

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Conecerto.Scoreboard.MixProject do
   def project do
     [
       app: :conecerto_scoreboard,
-      version: "0.9.0-rc.14",
+      version: "0.9.0-rc.15",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
